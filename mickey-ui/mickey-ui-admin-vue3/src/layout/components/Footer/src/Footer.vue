4b6cdfba2f40bf9f2e57166bd622df4bf8b1cd5d
329623516@qq.com <M520123M>
2023-02-09 12:09:33 +0800

    首页添加ICP备案信息

--- mickey-ui/mickey-ui-admin-vue3/src/layout/components/Footer/src/Footer.vue
+++ mickey-ui/mickey-ui-admin-vue3/src/layout/components/Footer/src/Footer.vue
@@ -17,6 +17,9 @@ const title = computed(() => appStore.getTitle)
     :class="prefixCls"
     class="text-center text-[var(--el-text-color-placeholder)] bg-[var(--app-contnet-bg-color)] h-[var(--app-footer-height)] leading-[var(--app-footer-height)] dark:bg-[var(--el-bg-color)]"
   >
-    <p style="font-size: 14px">Copyright ©2022-{{ title }}</p>
+    <p style="font-size: 14px"
+      >Copyright ©2023-{{ title }} 版权所有 |
+      <a href="https://beian.miit.gov.cn" target="_blank">陕ICP备2023000880号-1</a>
+    </p>
   </div>
 </template>
